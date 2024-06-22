7067094106ffe205da1481783079dd65979aa646
Khushal <khushalmajoka21@gmail.com>
2024-06-22 23:08:28 +0530

    implemented main func: added routes and handlers for different endpoints

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"url-shortener/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/shorten", handlers.ShortenURL)
+	router.GET("/:shortURL", handlers.RedirectURL)
+	router.GET("/metrics", handlers.GetMetrics)
+
+	router.Run(":8080")
+}
